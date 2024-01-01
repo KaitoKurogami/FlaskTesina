3e91ae730e11f4dabc98b7d362002fda500ea14d
GARDELLA RUIZ, ANDRES MAURO <andresmgr@gmail.com>
2024-01-01 12:29:50 -0300

    commit

---
+++ LIME.py
@@ -0,0 +1,135 @@
+import tensorflow as tf
+import numpy as np
+import copy
+from tensorflow import keras
+#from keras.applications.imagenet_utils import decode_predictions
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+#from keras.applications.vgg16 import preprocess_input,decode_predictions
+import skimage.io
+import skimage.segmentation
+import copy
+import sklearn
+import sklearn.metrics
+from sklearn.linear_model import LinearRegression
+import warnings
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from tqdm import tqdm
+from PIL import Image
+
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (6, 6)
+#para que elimine las lineas de los ejes deberia de cambiar cada uno a False por separado
+plt.axis('off')
+
+import cv2
+
+#image_path = "./tesina-imagenes/"
+#img_base_path = image_path + "perro.jpg"
+
+#print('Notebook running: keras ', keras.__version__)
+np.random.seed(222)
+
+
+def LIME(model,image,preprocessor, decoder, savefile_path, LIME_num_perturb,LIME_kernel_size=2.5,LIME_max_dist=224/8, LIME_ratio=0.3):
+
+  num_perturb=LIME_num_perturb
+
+  decode_predictions=decoder
+
+#preprocess image, data from cv2 is to be processed by the CNN
+  img = image
+  img = cv2.imread(img)
+  img = cv2.resize(img,(224,224))
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+  Xi=copy.deepcopy(img)
+
+#images from skimage are to make te superpixels and to show results graphically
+  X2 = skimage.io.imread(image)
+  X2 = skimage.transform.resize(X2, (224,224))
+
+  X = np.expand_dims(img, axis=0).astype(np.float32)
+  preprocess_input=preprocessor
+  X = preprocess_input(X)
+
+  preds = model.predict(X,verbose=0)
+  #print("Predicted:", decode_predictions(preds, top=1)[0])
+  result = decode_predictions(preds)
+  for res in result[0]:
+    print(res)
+
+  top_pred_classes = preds[0].argsort()[-5:][::-1]
+  top_pred_classes                #Index of top 5 classes
+
+  superpixels = skimage.segmentation.quickshift(X2, kernel_size=LIME_kernel_size,max_dist=LIME_max_dist, ratio=LIME_ratio)
+  num_superpixels = np.unique(superpixels).shape[0]
+  print(str(num_superpixels)+" superpixeles")
+
+  skimage.io.imshow(skimage.segmentation.mark_boundaries(Xi, superpixels))
+
+  perturbations = np.random.binomial(1, 0.5, size=(num_perturb, num_superpixels))
+
+  def perturb_image(img,perturbation,segments):
+    active_pixels = np.where(perturbation == 1)[0]
+    mask = np.zeros(segments.shape)
+    for active in active_pixels:
+        mask[segments == active] = 1
+    perturbed_image = copy.deepcopy(img)
+    perturbed_image = perturbed_image*mask[:,:,np.newaxis]
+    return perturbed_image
+
+
+  predictions = []
+  for pert in tqdm(perturbations):
+    perturbed_img = perturb_image(Xi,pert,superpixels)
+    perturbed_img = np.expand_dims(perturbed_img, axis=0).astype(np.float32)
+    perturbed_img = preprocess_input(perturbed_img)
+    pred = model.predict(perturbed_img,verbose=0)
+    result = decode_predictions(pred)
+    #for res in result[0]:
+      #print(res)
+    predictions.append(pred)
+
+  predictions = np.array(predictions)
+
+  original_image = np.ones(num_superpixels)[np.newaxis,:] #Perturbation with all superpixels enabled
+  distances = sklearn.metrics.pairwise_distances(perturbations,original_image, metric='cosine').ravel()
+  distances.shape
+
+  kernel_width = 0.25
+  weights = np.sqrt(np.exp(-(distances**2)/kernel_width**2)) #Kernel function
+  weights.shape
+
+  class_to_explain = top_pred_classes[0]
+  simpler_model = LinearRegression()
+  simpler_model.fit(X=perturbations, y=predictions[:,:,class_to_explain], sample_weight=weights)
+  coeff = simpler_model.coef_[0]
+
+  if int(num_superpixels*0.1)<7:
+    num_top_features=7
+  else:
+    num_top_features=int(num_superpixels*0.1)
+  top_features = np.argsort(coeff)[-num_top_features:]
+  top_features
+
+  mask = np.zeros(num_superpixels)
+  mask[top_features]= True #Activate top superpixels
+
+
+  #return perturb_image(X2,mask,superpixels)
+
+  imagen_resultado=perturb_image(X2,mask,superpixels)
+  skimage.io.imsave(savefile_path,np.array(Image.fromarray((imagen_resultado * 255).astype(np.uint8))))
+
+  print("image saved at: "+savefile_path)
+
+
+image_path = "./tesina-imagenes/random/"
+#namelist=["EntleBucher.jpg","dutch.jpg","Macaw.jpg","Maze.jpg","forklift.jpg","toucan.jpg","weasel.jpg"]
+namelist=["abacus.jpg","baseball.jpg","dumbbell.jpg","microphone.jpg","tostadora2.jpg"]
+#namelist=["abacus.jpg"]
+for name in namelist:
+  img_base_path = image_path + name
+  ubicacion="Resultados/random/"+"LIMERes"+name
+  LIME(tf.keras.applications.ResNet50(weights="imagenet"),img_base_path,preprocess_input,decode_predictions, ubicacion,1000,LIME_max_dist=224/8)
---
+++ gradCAM.py
@@ -0,0 +1,153 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+# Display
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (6, 6)
+#para que elimine las lineas de los ejes deberia de cambiar cada uno a False por separado
+plt.axis('off')
+import cv2
+"""
+
+"""
+
+
+image_path = "./tesina-imagenes/"
+img_base_path = image_path + "1002.jpg"
+
+def GRADCAM(model, last_conv_layer,image,scale,preprocessor, decoder, savefile_path):
+  from IPython.display import Image, display
+  from scipy.ndimage import zoom
+
+  def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+      # First, we create a model that maps the input image to the activations
+      # of the last conv layer as well as the output predictions
+      grad_model = tf.keras.models.Model(
+          [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+      )
+
+      # Then, we compute the gradient of the top predicted class for our input image
+      # with respect to the activations of the last conv layer
+      with tf.GradientTape() as tape:
+          last_conv_layer_output, preds = grad_model(img_array)
+          if pred_index is None:
+              pred_index = tf.argmax(preds[0])
+          class_channel = preds[:, pred_index]
+
+      # This is the gradient of the output neuron (top predicted or chosen)
+      # with regard to the output feature map of the last conv layer
+      grads = tape.gradient(class_channel, last_conv_layer_output)
+
+      # This is a vector where each entry is the mean intensity of the gradient
+      # over a specific feature map channel
+      pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+      #print(pooled_grads)
+
+      # We multiply each channel in the feature map array
+      # by "how important this channel is" with regard to the top predicted class
+      # then sum all the channels to obtain the heatmap class activation
+
+      last_conv_layer_output = last_conv_layer_output[0]
+      heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+      heatmap = tf.squeeze(heatmap)
+
+      # For visualization purpose, we will also normalize the heatmap between 0 & 1
+      # ademas, al utilizar el tf.maximum(heatmap,0) seria la parte del ReLU
+      # Ya que el Relu se tiene que aplicar sobre el resultado de la combinacion lineal de los mapas de activaciones
+      # siendo ese resultado el heatmap calculado en las lineas previas
+      # nos interesa la influencia positiva en la clase de interes
+      # pero esto es solo para la representacion visual final
+      heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+
+      global conv
+      global weights
+      weights = pooled_grads[..., tf.newaxis]
+      conv=np.array(last_conv_layer_output)
+      return heatmap.numpy()
+
+  conv=[]
+  weights=[]
+
+  img = cv2.imread(image)
+  img = cv2.resize(img,(224,224))
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+  X = np.expand_dims(img, axis=0).astype(np.float32)
+  X = preprocess_input(X)
+
+  np.random.seed(222)
+
+  # Print what the top predicted class is
+  preds = model.predict(X,verbose=0)
+  print("Predicted:", decode_predictions(preds, top=1)[0])
+  result = decode_predictions(preds)
+  for res in result[0]:
+    print(res)
+
+  # Remove last layer's softmax
+  model.layers[-1].activation = None
+  # Generate class activation heatmap
+  heatmap = make_gradcam_heatmap(X, model, last_conv_layer_name)
+
+  # Display heatmap
+  #plt.matshow(heatmap)
+  #plt.show()
+
+  def save_and_display_gradcam(img_path, heatmap, cam_path="cam.jpg", alpha=0.5):
+      # Load the original image
+      img = keras.utils.load_img(img_path)
+      img = keras.utils.img_to_array(img)
+
+      # Rescale heatmap to a range 0-255
+      heatmap = np.uint8(255 * heatmap)
+
+      # Use jet colormap to colorize heatmap
+      jet = cm.get_cmap("jet")
+
+      # Use RGB values of the colormap
+      jet_colors = jet(np.arange(256))[:, :3]
+      jet_heatmap = jet_colors[heatmap]
+
+      # Create an image with RGB colorized heatmap
+      jet_heatmap = keras.utils.array_to_img(jet_heatmap)
+      jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+      jet_heatmap = keras.utils.img_to_array(jet_heatmap)
+
+      # Superimpose the heatmap on original image
+      superimposed_img = jet_heatmap * alpha + img
+      superimposed_img = keras.utils.array_to_img(superimposed_img)
+
+      # Save the superimposed image
+      superimposed_img.save(cam_path)
+
+      # Display Grad CAM
+      display(Image(cam_path))
+
+  plt.figure(figsize=(12, 6))
+  a = plt.imshow(img)
+  plt.imshow(zoom(heatmap, zoom=(scale, scale)), cmap='jet', alpha=0.5)
+  plt.savefig(savefile_path,bbox_inches='tight')
+  print("image saved at: "+savefile_path)
+  plt.clf()
+
+
+seleccion=2
+if seleccion == 1:
+  from keras.applications.vgg16 import preprocess_input,decode_predictions
+  last_conv_layer_name = "block5_conv3"
+  scale = 224 / 14
+elif seleccion == 2:
+  from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+  last_conv_layer_name = "conv5_block3_out"
+  scale = 224 / 7
+else:
+  print("elegir 1 o 2")
+
+image_path = "./static/files/results"
+for name in namelistImagenet:
+  img_base_path = image_path + name
+  ubicacion="imagenet/GRADCAMRes"+name
+  #GRADCAM(tf.keras.applications.VGG16(weights="imagenet"),"block5_conv3",img_base_path,224/14,preprocess_input,decode_predictions, ubicacion)
+  GRADCAM(tf.keras.applications.ResNet50(weights="imagenet"),last_conv_layer_name,img_base_path,scale,preprocess_input,decode_predictions, ubicacion)
